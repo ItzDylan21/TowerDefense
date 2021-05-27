1b0be6655e2828729ac3ae549159b05f667f268a
Hyumari <78027104+Hyumari@users.noreply.github.com>
2021-05-27 10:47:42 +0200

    test

--- rocdefence/main.lua
+++ rocdefence/main.lua
@@ -1,1 +1,3 @@
-local background = display.newImageRect( "images/level1.png", 360, 570 )
+local background = display.newImageRect( "images/level1.png", 1467, 683 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
