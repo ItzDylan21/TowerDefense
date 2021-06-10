aaca6918dd3a68603cabd71fffe04e9b8ca87f84
Hyumari <78027104+Hyumari@users.noreply.github.com>
2021-06-10 11:25:40 +0200

    Update main.lua

--- rocdefence/main.lua
+++ rocdefence/main.lua
@@ -43,3 +43,30 @@ local function rechtsfunctie2()
 	transition.to (background, {x=background.x - 30})
 end
 rechtsfunctie2()
+
+
+
+local player1 = display.newImageRect( "sprites/player/player1.png", 50, 50 )
+player1.x = display.contentCenterX
+player1.y = display.contentCenterY
+
+
+local function dragPlayer1( event )
+ 
+    local player1 = event.target
+    local phase = event.phase
+    if ( "began" == phase ) then
+        display.currentStage:setFocus( player1 )
+        player1.touchOffsetX = event.x - player1.x
+        player1.touchOffsetY = event.y - player1.y
+    elseif ( "moved" == phase ) then
+        player1.x = event.x - player1.touchOffsetX
+        player1.y = event.y - player1.touchOffsetY
+    elseif ( "ended" == phase or "cancelled" == phase ) then
+        display.currentStage:setFocus( nil )
+        return true  -- Prevents touch propagation to underlying objects
+    end
+
+end
+
+player1:addEventListener( "touch", dragPlayer1 )
