a2d12921042bd74c7f0a1865a8b55f4862d907e4
Dylan Worseling <dylan-dylan99@hotmail.com>
2021-06-10 11:20:58 +0200

    ...

--- rocdefence/main.lua
+++ rocdefence/main.lua
@@ -2,6 +2,14 @@ local screenTop
 local screenBottom
 local screenLeft
 local screenRight
+local count = 0
+local spawnParams = {
+    x = -100,
+    y = 300,
+    spawnTime = 200,
+}
+local enemyTable = {}
+
 
 -- Get current edges of visible screen
 screenTop = display.screenOriginY
@@ -14,12 +22,27 @@ local background = display.newImageRect("images/level1.png", (1.33 * screenRight
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local enemy = display.newImageRect("images/circle.png", 100, 100)
-enemy.x = -100
-enemy.y = 300
+
+
+
+local function createEnemy()
+ 
+  local enemy = display.newImageRect("images/circle.png", 100, 100)
+  table.insert( enemyTable, enemy )
+  enemy.myName = "enemy"
+
+  enemy.x = -100
+  enemy.y = 300
+  transition.to(enemy, {delay=1000, time=10000, x=enemy.x + 1200} )
+end
 
 local function rechtsfunctie()
-	transition.to(enemy, {time=10000, x=enemy.x+ 1200})
+	transition.to (enemy, {time=10000, x=enemy.x+ 1200})
 	transition.to (background, {x=background.x - 30})
 end
-rechtsfunctie()
+while (count < 10) do
+	createEnemy()
+	count = count + 1
+end
+
+
